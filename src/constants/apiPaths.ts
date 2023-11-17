c8f4e6fa0f47761be25e0e77690c4346d87cc442
Iryna <iryna.kolhanova@gmail.com>
2023-11-17 11:05:42 -0500

    fix: add aws url from backend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://u9fr8n434j.execute-api.us-east-2.amazonaws.com/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
