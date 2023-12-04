d8b607a371b111593a2850aabfc25ec44e54f513
Iryna <iryna.kolhanova@gmail.com>
2023-12-04 08:55:49 -0500

    feat: refactor FE accoding task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://u9fr8n434j.execute-api.us-east-2.amazonaws.com/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://h6dq0q7bf6.execute-api.us-east-2.amazonaws.com/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
